085f4d2c6cf5c5de8091e1dda3a3bb030668f766
Kingsley Owusu-Sekyere <k.owususekyere.86@gmail.com>
2022-03-18 15:30:23 +0100

    add innerHtml

--- projects/ngx-tour-guide/src/lib/models/tour-guide-stop.model.ts
+++ projects/ngx-tour-guide/src/lib/models/tour-guide-stop.model.ts
@@ -3,77 +3,86 @@ import { Observable } from 'rxjs';
 
 export type Direction = 'top' | 'right' | 'bottom' | 'left';
 export type Alignment = 'start' | 'end' | 'center';
-export type NumericMargin = number | [number, number] | [number, number, number] | [number, number, number, number];
+export type NumericMargin =
+  | number
+  | [number, number]
+  | [number, number, number]
+  | [number, number, number, number];
 
 /**
  * Data of a single stop
  */
 export interface TourGuideStop {
+  /**
+   * Can be used to target a domElement for the stop
+   * reference of an element or a query selector
+   */
+  element?: HTMLElement | string;
+
+  /**
+   * Content of the stop to print as message or use as implict context for a ng-template
+   */
+  content: string | any;
+
+  /**
+   * Template ref to use instead of string content
+   */
+  contentTemplate?: TemplateRef<any>;
+
+  /**
+   * Uses the content as inline html, 
+   */
+  useHtml?: boolean;
+
+  /**
+   * Title of the step
+   */
+  title?: string;
+
+  /**
+   * Margin value to adjust the element region in px
+   */
+  margin?: NumericMargin;
+
+  /**
+   * Css expression for max-width
+   */
+  maxWidth?: string;
+
+  /**
+   * Rendererd HTML code
+   */
+  renderedHtml?: string;
+
+  /**
+   * Callback before entering a stop via "next" or "previous"
+   */
+  onEnter?: () => void;
+
+  /**
+   * Callback before "nexting" a stop
+   */
+  onLeave?: () => void;
+
+  /**
+   * Define an action as trigger for next step
+   */
+  action?: {
     /**
-     * Can be used to target a domElement for the stop
-     * reference of an element or a query selector
+     * If true a stop with an action won't show the next or finish button
      */
-    element?: HTMLElement | string;
+    replacesNextOrFinish?: boolean;
 
     /**
-     * Content of the stop to print as message or use as implict context for a ng-template
+     * Event to trigger the next (or finish action if step is last).
+     * In case of string the event is an event listened on the stops element if defined.
+     * In case of an observable the step will be nexted on emit of the observable
      */
-    content: string | any;
+    event?: keyof HTMLElementEventMap | Observable<unknown>;
 
     /**
-     * Template ref to use instead of string content
+     * Execution delay of next step after trigger
      */
-    contentTemplate?: TemplateRef<any>;
-
-    /**
-     * Title of the step
-     */
-    title?: string;
-
-    /**
-     * Margin value to adjust the element region in px
-     */
-    margin?: NumericMargin;
-
-    /**
-     * Css expression for max-width
-     */
-    maxWidth?: string;
-
-    /**
-     * Rendererd HTML code
-     */
-    renderedHtml?: string;
-
-    /**
-     * Callback before entering a stop via "next" or "previous"
-     */
-    onEnter?: () => void;
-
-    /**
-     * Callback before "nexting" a stop
-     */
-    onLeave?: () => void;
-
-    /**
-     * Define an action as trigger for next step
-     */
-    action?: {
-        /**
-         * If true a stop with an action won't show the next or finish button
-         */
-        replacesNextOrFinish?: boolean;
-
-        /**
-         * Event to trigger the next (or finish action if step is last).
-         * In case of string the event is an event listened on the stops element if defined.
-         * In case of an observable the step will be nexted on emit of the observable
-         */
-        event?: (keyof HTMLElementEventMap) | Observable<unknown>;
-
-        /**
-         * Execution delay of next step after trigger
-         */
-        delay?: number;
-    };
+    delay?: number;
+  };
 }
